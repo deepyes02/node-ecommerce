61ae2ed356a9f42da949e1d4de78341ba9c3dd76
deepyes02 <deepyes@outlook.com>
2020-09-27 09:32:36 +0545

    another one

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,42 +1,67 @@
 const Product = require('../models/product');
-
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
-};
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+    Product.fetchAll(products => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/'
+        });
     });
-  });
 };
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products'
+        });
+    });
+};
+
+exports.getSingleProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (product !== undefined) {
+            console.log(product);
+            res.render("shop/product-detail", {
+                product: product,
+                pageTitle: product.title,
+                path: '/singleProduct'
+            })
+        } else {
+            res.send("Sorry, no product found!");
+        }
+    });
+}
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+    res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart'
+    });
 };
 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    })
+    res.redirect("/cart");
+}
+
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,19 @@
-const path = require('path');
-
 const express = require('express');
-
 const shopController = require('../controllers/shop');
-
 const router = express.Router();
+
+//dynamic router comes below the main router. for eg:
+// router.get(product/all);
+// router.get(product/:id);
 
 router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get('/products', shopController.getProducts); //static route comes before
+
+router.get('/products/:productId', shopController.getSingleProduct); //dynamic routes come after
 
 router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
