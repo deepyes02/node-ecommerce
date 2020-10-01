dd52173a6a1d093c615c373313d52cfe4dddf12f
deepyes02 <deepyes@outlook.com>
2020-10-01 23:03:42 +0545

    mongodb implementation, adding products page now works along with 404 so that the site loads

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,15 @@
 const Product = require('../models/product');
 
 
-exports.getProducts = (req, res, next) => {
-    Product.findAll().then(products=>{
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products'
-        });
-    }).catch(err=>console.log(err));       
-};
+// exports.getProducts = (req, res, next) => {
+//     Product.findAll().then(products=>{
+//         res.render('admin/products', {
+//             prods: products,
+//             pageTitle: 'Admin Products',
+//             path: '/admin/products'
+//         });
+//     }).catch(err=>console.log(err));       
+// };
 
 
 exports.getAddProduct = (req, res, next) => {
@@ -25,63 +25,58 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    req.user
-        .createProduct({
-            title: title,
-            imageUrl:imageUrl,
-            price:price,
-            description:description
-        })
+    const product = new Product(title,price,description,imageUrl);
+    product.save()
         .then(result=>{
-            console.log(result);
-            res.redirect("/admin/products");
+            console.log('product created');
+            res.redirect("/");
         })
-        .catch(err=>console.log(err));
+        .catch(err=>{console.log(err)})
 };
 
-exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
-    if (!editMode) {
-        return res.redirect("/");
-    }
-    const prodId = req.params.productId;
-    Product.findByPk(prodId)
-    .then(product=> {
-        if (!product) {
-            return res.redirect("/");
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
-        });
-    }).catch(err=>console.log(err))
-};
+// exports.getEditProduct = (req, res, next) => {
+//     const editMode = req.query.edit;
+//     if (!editMode) {
+//         return res.redirect("/");
+//     }
+//     const prodId = req.params.productId;
+//     Product.findByPk(prodId)
+//     .then(product=> {
+//         if (!product) {
+//             return res.redirect("/");
+//         }
+//         res.render('admin/edit-product', {
+//             pageTitle: 'Edit Product',
+//             path: '/admin/edit-product',
+//             editing: editMode,
+//             product: product
+//         });
+//     }).catch(err=>console.log(err))
+// };
 
-exports.postEditProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    const updatedTitle = req.body.title;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDescription = req.body.description;
-    const updatedPrice = req.body.price;
+// exports.postEditProduct = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     const updatedTitle = req.body.title;
+//     const updatedImageUrl = req.body.imageUrl;
+//     const updatedDescription = req.body.description;
+//     const updatedPrice = req.body.price;
 
-    Product.findOne({where: {id:prodId}})
-    .then(product => {
-        product.title = updatedTitle;
-        product.imageUrl = updatedImageUrl;
-        product.description = updatedDescription;
-        product.price = updatedPrice;
-        product.save();
-    }).then(()=>{
-        res.redirect("/admin/products");
-    }).catch(err=>console.log(err));
-}
+//     Product.findOne({where: {id:prodId}})
+//     .then(product => {
+//         product.title = updatedTitle;
+//         product.imageUrl = updatedImageUrl;
+//         product.description = updatedDescription;
+//         product.price = updatedPrice;
+//         product.save();
+//     }).then(()=>{
+//         res.redirect("/admin/products");
+//     }).catch(err=>console.log(err));
+// }
 
-exports.postDeleteProduct = (req,res,next)=>{
-    const prodId = req.body.productId;
-    Product.destroy({where: {id:prodId}}).then(()=>{
-        res.redirect("/admin/products");
-    }).catch(err=>console.log(err));
-}
+// exports.postDeleteProduct = (req,res,next)=>{
+//     const prodId = req.body.productId;
+//     Product.destroy({where: {id:prodId}}).then(()=>{
+//         res.redirect("/admin/products");
+//     }).catch(err=>console.log(err));
+// }
 
--- util/database.js
+++ util/database.js
@@ -1,12 +1,29 @@
-const Sequelize = require('sequelize').Sequelize
-const sequelize = new Sequelize(
-    'node_udemy',
-    'root',
-    '736852',
-    {
-        dialect: 'mysql',
-        host: 'localhost'
-    }
-);
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const uri = "mongodb+srv://deepyes02:j3kbzaEhes0jjbQ3@deepyes02.qegbb.mongodb.net/nosql_node_udemy?retryWrites=true&w=majority";
+
+let _db;
+const mongoConnect = callback => {
+
+MongoClient.connect(uri, {useUnifiedTopology:true})
+    .then(client=>{
+        console.log('Connected');
+        _db = client.db();
+    })
+    .catch(err=>{
+        console.log(err)
+        throw err;
+    });
+}
+
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database Found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
